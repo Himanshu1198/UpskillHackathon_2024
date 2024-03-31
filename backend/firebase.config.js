fbfbdc00aafb70382a4a6de7933d4f8edc8bc6b7
bhumitmehta <bhumitmehta2209@gmail.com>
2024-03-31 17:16:41 +0530

    firebaseConfig

---
+++ backend/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsSnBFiOwg2bUqYmXr13t7ATpharzq_kY",
+  authDomain: "mentorconnect-45d31.firebaseapp.com",
+  databaseURL: "https://mentorconnect-45d31-default-rtdb.firebaseio.com",
+  projectId: "mentorconnect-45d31",
+  storageBucket: "mentorconnect-45d31.appspot.com",
+  messagingSenderId: "231287460946",
+  appId: "1:231287460946:web:20c89928949a0ce9267b8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
