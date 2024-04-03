d7cc61de8e32fe257e8e1fc96428c7fc5019ef46
Dhruv <dhruv.nariani@gmail.com>
2024-04-03 18:52:06 +0530

    Dhruv

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const dotenv=require('dotenv').config()
 const connectDB=require('./config/db')
 const {errorHandler}=require('./middleware/errorMiddleware')
 const PORT =process.env.PORT ||8000
-// const clientid="467342628248-lj5i1tssia0iau9b51i36g7le234lp64.apps.googleusercontent.com"
-// const clientsecret="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"
 connectDB()
 const app=express()
 
